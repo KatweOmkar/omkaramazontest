3cf603835e1e26643e31cfb5a2c933b841e9cec3
Katwe <omkar.katwe@hp.com>
2021-12-24 01:08:33 +0530

    adding amazon test cases

---
+++ support/Page_objects/amazondata.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+var Amazondata = {
+
+
+    inputdata : {
+        username: 'omkarkatwe154@gmail.com',
+        password : 'Om@amazon7',
+        itemname : 'Nokia G20',
+        //password : 'Om@trip7',
+        hotelname : 'Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in',
+    }
+}
+
+module.exports = Amazondata;
---
+++ support/Page_objects/amazonpom.js
@@ -0,0 +1,23 @@
+var POM={
+
+
+
+    homepage:{
+
+        signinicon: '#nav-link-accountList',
+        emailinput : '#ap_email',
+        passwordinput : '#ap_password',
+        continuebtn : '[aria-labelledby="continue-announce"]',
+        signinbtn: '#signInSubmit',
+        searchinput: '[aria-label="Search"]',
+        searchicon: '[value="Go"]',
+        searcheditem : '#search div:nth-child(3) div:nth-child(2) h2 span',
+        addtocartbtn : '[aria-labelledby="submit.add-to-cart-announce"]',
+        skipbtn : '[aria-labelledby="attachSiNoCoverage-announce"]',
+        carticon : '#nav-cart',
+        itemtext : '[data-a-overflow-marker="&hellip;"]',
+
+    }
+}
+
+module.exports = POM;
